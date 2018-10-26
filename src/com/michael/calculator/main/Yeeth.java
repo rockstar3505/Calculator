c928bac09d27de1346852e443a3551af80a9f0a3
Daniel Chapin <dchapes123@gmail.com>
2018-10-26 18:03:47 -0500

    Revert "Created basic swing window for calculator"

--- src/com/michael/calculator/main/Yeeth.java
+++ src/com/michael/calculator/main/Yeeth.java
@@ -1,29 +1,10 @@
 package com.michael.calculator.main;
 
-import java.awt.GridLayout;
+public class Yeeth {
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Yeeth extends JFrame {
-
-	JPanel panel = new JPanel();
-	
 	public static void main(String[] args) {
-		Yeeth yeeth = new Yeeth();
+		// TODO Auto-generated method stub
 
-	}
-	
-	Yeeth() {
-		setTitle("Calculator");
-		setSize(480, 600);
-		
-		panel.setLayout(new GridLayout(4, 6));
-		
-		getContentPane().add(panel);
-		
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 }
