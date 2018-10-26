741f099ba15689a7b49dab88c12583beb3284157
Daniel Chapin <dchapes123@gmail.com>
2018-10-26 18:00:42 -0500

    Created basic swing window for calculator
    provided new GridLayout.
    declared panel.
    basic setup.

--- src/com/michael/calculator/main/Yeeth.java
+++ src/com/michael/calculator/main/Yeeth.java
@@ -1,10 +1,29 @@
 package com.michael.calculator.main;
 
-public class Yeeth {
+import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Yeeth extends JFrame {
+
+	JPanel panel = new JPanel();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Yeeth yeeth = new Yeeth();
 
+	}
+	
+	Yeeth() {
+		setTitle("Calculator");
+		setSize(480, 600);
+		
+		panel.setLayout(new GridLayout(4, 6));
+		
+		getContentPane().add(panel);
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 }
